19afd9b2d1c71c3a1917f5786a0adc206f569193
Balaji Kamble <akashkamble965@gmail.com>
2023-12-06 17:41:26 +0530

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function Sidebar() {
+  return (
+    <div className='d-flex justify-content-between flex-column bg-dark text-white'>
+      <div>
+        <a href=''>
+          <i className='bi bi-code-slash'></i>
+        </a>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
